720bca8b791b4d0e5c1cfecf5b93e5883d29785c
Nikola <dzonza996@gmail.com>
2024-08-28 14:55:49 +0200

    adding nav scroll animation

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -3,8 +3,10 @@ const aboutMeTitle = document.querySelector('.about-me-title');
 const projectsList = document.querySelectorAll('.project-articles');
 const contactTitle = document.querySelector('.contact-animation-title');
 const contactDetails = document.querySelectorAll('.contact-animation-details');
+const header = document.querySelector('header');
+const marker = document.getElementById('scroll-marker');
 
-const moveLeft = function (entries) {
+const moveUp = function (entries) {
   entries.forEach((entry) => {
     const element = entry.target;
     const animation = element.getAttribute('data-animation');
@@ -16,7 +18,7 @@ const moveLeft = function (entries) {
   });
 };
 
-const observer = new IntersectionObserver(moveLeft, {
+const observer = new IntersectionObserver(moveUp, {
   threshold: [0, 0.5],
 });
 
@@ -25,3 +27,19 @@ observer.observe(aboutMeTitle);
 projectsList.forEach((project) => observer.observe(project));
 observer.observe(contactTitle);
 contactDetails.forEach((detail) => observer.observe(detail));
+
+// scroll nav
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) header.classList.add('header-scroll');
+  else header.classList.remove('header-scroll');
+};
+
+const mainSectionObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+
+mainSectionObserver.observe(marker);
